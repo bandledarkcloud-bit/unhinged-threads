30520b2bc8f3890852ec59c89d1f0d960a420ab1
Jason <jason@localhost>
2026-06-03 00:33:25 -0500

    Remove Add to Cart buttons from Shop All and Weekly Drop

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -36,14 +36,8 @@ export default function ShopPage() {
                   </div>
                 </Link>
                 
-                <div className="mt-auto pt-3 flex items-center justify-between">
+                <div className="mt-auto pt-3">
                   <div className="text-2xl font-black">${product.price}</div>
-                  <Link 
-                    href={`/shop/${product.slug}`}
-                    className="px-2.5 py-0.5 bg-white text-black text-[9px] font-black inline-flex items-center justify-center active:bg-[#ff0088] active:text-white transition-all"
-                  >
-                    ADD TO CART
-                  </Link>
                 </div>
               </div>
             </div>
